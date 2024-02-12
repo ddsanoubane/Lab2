97a93e349861a41f2f432f5eec28c58bd5505845
ddsanoubane <151870714+ddsanoubane@users.noreply.github.com>
2024-02-11 23:47:15 -0500

    Lab2.py

---
+++ Lab2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#Description of Function: A function that uses command line arguements. This function will print two variables used, the script and then the script and variables.
+import sys
+
+def print_variables():
+    script_variable = sys.argv[0]
+
+    print('Script name: {script_variable}')
+
+    if len(sys.argv) > 1:
+        arguments = ' '.join(sys.argv[1:])
+        print(f"Script and arguments: {script_variable} {arguments}")
+    else:
+        print("No arguments used with the script.")
+
+print_variables()
